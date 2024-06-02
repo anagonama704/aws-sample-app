eadd2d8e90af661135205cb18beb201cfe9eb81f
kei <dr.kei21@gmail.com>
2024-06-02 21:09:10 +0900

    fix: タイトルの変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -415,7 +415,7 @@
                 </svg>
 
             </div>
-            <h3 style="color:red">あなごなま</h3>
+            <h3 style="color:red">テストです</h3>
 
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                 <div class="grid grid-cols-1 md:grid-cols-2">
